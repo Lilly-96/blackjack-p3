61e4b1df49cb5fefcbf60b8e648f2243a512d0cc
Lilly H <lilianholtenvall@live.se>
2023-07-15 20:17:36 +0000

    added Rules

--- run.py
+++ run.py
@@ -8,3 +8,18 @@ DIAMONDS = chr(9830) # Character 9830 is '♦'.
 SPADES = chr(9824) # Character 9824 is '♠'.
 CLUBS = chr(9827) # Character 9827 is '♣'.
 BACKSIDE = 'backside' 
+
+def main():
+    print('''Blackjack
+
+    Rules:
+      Try to get as close to 21 without going over.
+      Kings, Queens, and Jacks are worth 10 points.
+      Aces are worth 1 or 11 points.
+      Cards 2 through 10 are worth their face value.
+       (H)it to take another card.
+       (S)tand to stop taking cards.
+       On your first play, you can (D)ouble down to increase your bet
+       but must hit exactly one more time before standing.
+       In case of a tie, the bet is returned to the player.
+       The dealer stops hitting at 17.''')
