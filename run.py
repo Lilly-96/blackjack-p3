b4a9d6c0398169ed15da6f6c7cdcfedad47966f4
Lilly H <lilianholtenvall@live.se>
2023-07-15 20:58:07 +0000

    added bet = int(bet)

--- run.py
+++ run.py
@@ -108,3 +108,34 @@ def main():
                money += bet
            elif playerValue == dealerValue:
                print('It\'s a tie, the bet is returned to you.')
+
+               input('Press Enter to continue...')
+               print('\n\n')
+
+               def getBet(maxBet):
+                   """Ask the player how much they want to bet for this round."""
+                   while True: # Keep asking until they enter a valid amount.
+                       print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+                       bet = input('> ').upper().strip()
+                       if bet == 'QUIT':
+                           print('Thanks for playing!')
+                           sys.exit()
+
+                        if not bet.isdecimal():
+                            continue # If the player didn't enter a number, ask again.
+
+                        bet = int(bet)
+                        if 1 <= bet <= maxBet:
+                            return bet # Player entered a valid bet.
+                       
+
+                       def getDeck():
+                           """Return a list of (rank, suit) tuples for all 52 cards."""
+                           deck = []
+                           for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+                               for rank in range(2, 11):
+                                   deck.append((str(rank), suit)) # Add the numbered cards.
+                               for rank in ('J', 'Q', 'K', 'A'):
+                                   deck.append((rank, suit)) # Add the face and ace cards.
+                           random.shuffle(deck)
+                           return deck
